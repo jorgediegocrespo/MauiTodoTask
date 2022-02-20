025c1681ca96469f7e2ff8c471f0761199cd485f
Jorge Diego <jorge@devsdna.com>
2022-02-20 13:10:28 +0100

    Created alert dialog service

---
+++ TaskManager/TaskManager/Services/AlertDialogService/IAlertDialogService.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManager.Services;
+
+public interface IAlertDialogService
+{
+    Task ShowDialogAsync(string title, string message, string close);
+    Task<bool> ShowDialogConfirmationAsync(string title, string message, string cancel, string ok);
+}
