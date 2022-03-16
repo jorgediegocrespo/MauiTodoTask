e112cbc8f926266191a29524dd2df9e62ce35dac
Jose Manuel Montero <jmmontero.ortega@gmail.com>
2022-03-16 18:05:19 +0100

    Creating testing project

---
+++ TaskManager/TaskManager.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Moq;
+using TaskManager.Features;
+using TaskManager.Services;
+using Xunit;
+
+namespace TaskManager.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    
+    }
+}
